f897a03eca375bdaaa089761d876fe98ce1cce9b
Julita Tura <pogojulita@gmail.com>
2023-05-11 12:09:42 +0200

    update question controller

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const PORT = 3000
 
 const app = express()
 
+const validateAnswerOrQuestion = body => {
+  const errors = []
+  if (!body.summary || typeof body.summary !== 'string')
+    errors.push('Summary should be provided in request body as string')
+
+  if (!body.author || typeof body.author !== 'string')
+    errors.push('Author should be provided in request body as string')
+
+  if (errors.length) {
+    throw new Error(errors.join(', '), { httpCode: 400 })
+  }
+}
+
 app.use(urlencoded({ extended: true }))
 app.use(json())
 app.use(makeRepositories(STORAGE_FILE_PATH))
@@ -20,15 +33,55 @@ app.get('/questions', async (req, res) => {
   res.json(questions)
 })
 
-app.get('/questions/:questionId', (req, res) => {})
+app.get('/questions/:questionId', async (req, res) => {
+  const question = await req.repositories.questionRepo.getQuestionById(
+    req.params.questionId
+  )
+  if (question) {
+    res.json(question)
+  } else {
+    throw new Error('Question not found', { httpCode: 404 })
+  }
+})
 
-app.post('/questions', (req, res) => {})
+app.post('/questions', async (req, res) => {
+  validateAnswerOrQuestion(req.body)
+  const newQuestion = await req.repositories.questionRepo.addQuestion(req.body)
+  res.status(201).json(newQuestion)
+})
 
-app.get('/questions/:questionId/answers', (req, res) => {})
+app.get('/questions/:questionId/answers', async (req, res) => {
+  const answers = await req.repositories.questionRepo.getAnswers(
+    req.params.questionId
+  )
+  res.json(answers)
+})
 
-app.post('/questions/:questionId/answers', (req, res) => {})
+app.post('/questions/:questionId/answers', async (req, res) => {
+  validateAnswerOrQuestion(req.body)
+  const newAnswer = await req.repositories.questionRepo.addAnswer(
+    req.params.questionId,
+    req.body
+  )
+  res.status(201).json(newAnswer)
+})
 
-app.get('/questions/:questionId/answers/:answerId', (req, res) => {})
+app.get('/questions/:questionId/answers/:answerId', async (req, res) => {
+  const answer = await req.repositories.questionRepo.getAnswerById(
+    req.params.questionId,
+    req.params.answerId
+  )
+  if (answer) {
+    res.json(answer)
+  } else {
+    throw new Error('Answer not found', { httpCode: 404 })
+  }
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(err.httpCode || 500).send(err.message || 'Unexpected Error')
+})
 
 app.listen(PORT, () => {
   console.log(`Responder app listening on port ${PORT}`)
--- repositories/question.js
+++ repositories/question.js
@@ -1,4 +1,5 @@
 const { readFile, writeFile } = require('fs/promises')
+const { v4: uuid } = require('uuid')
 
 const makeQuestionRepository = fileName => {
   const _getQuestionsFromFile = async () => {
@@ -16,13 +17,14 @@ const makeQuestionRepository = fileName => {
 
   const getQuestionById = async questionId => {
     const questions = await _getQuestionsFromFile()
-    return questions.find(({ id }) => id === questionId)
+    return questions.find(({ id }) => id === questionId) || null
   }
 
   const addQuestion = async question => {
     const questions = await getQuestions()
-    questions.push(question)
+    questions.push({ ...question, id: uuid() })
     await _saveQuestionsToFile(questions)
+    return question
   }
 
   const getAnswers = async questionId => {
@@ -32,15 +34,16 @@ const makeQuestionRepository = fileName => {
 
   const getAnswer = async (questionId, answerId) => {
     const answers = await getAnswers(questionId)
-    return answers.find(({ id }) => id === answerId)
+    return answers.find(({ id }) => id === answerId) || null
   }
 
   const addAnswer = async (questionId, answer) => {
     const questions = await _getQuestionsFromFile()
     const questionToUpdate = questions.find(({ id }) => id === questionId)
-    questionToUpdate.answers.push(answer)
+    questionToUpdate.answers.push({ ...answer, id: uuid() })
     // answer saved in questions array by reference
     await _saveQuestionsToFile(questions)
+    return answer
   }
 
   return {
